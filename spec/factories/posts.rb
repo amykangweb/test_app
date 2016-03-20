a114f0de4c01e2bc40429ec1a95b225cbd639e17
amykangweb <selene6023@gmail.com>
2016-03-20 00:08:52 -0700

    Write feature create spec for posts.

---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :first, class: Post do
+    title "First Post"
+    content "This is my first post."
+  end
+end
---
+++ spec/features/create_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe "Creating Post" do
+  before do
+    @post = FactoryGirl.build(:first)
+    @post.save
+  end
+
+  it "redirects to the post page on success" do
+    visit '/posts'
+    click_link "New Post"
+    fill_in "Title", with: @post.title
+    fill_in "Content", with: @post.content
+    click_button "Create Post"
+    expect(page).to have_content(@post.title)
+    expect(page).to have_content(@post.content)
+  end
+
+  it "shows error message when title is blank" do
+    visit '/posts'
+    click_link "New Post"
+    fill_in "Title", with: ""
+    fill_in "Content", with: @post.content
+    click_button "Create Post"
+    expect(page).to have_content("Title cannot be blank.")
+  end
+
+  it "shows error message when content is blank" do
+    visit '/posts'
+    click_link "New Post"
+    fill_in "Title", with: @post.title
+    fill_in "Content", with: ""
+    click_button "Create Post"
+    expect(page).to have_content("Content cannot be blank.")
+  end
+end
