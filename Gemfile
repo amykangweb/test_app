b94e29ad86edc9b41f5df83f62806db976b30e53
amykangweb <selene6023@gmail.com>
2016-03-20 01:06:01 -0700

    Add performance tests for rendering homepage and creating a post.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'jquery-rails'
 gem 'rails', '4.2.5.1'
 gem 'rails-perftest'
+gem 'ruby-prof'
 gem 'sass-rails', '~> 5.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'sqlite3'
---
+++ test/performance/post_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+require 'rails/performance_test_help'
+
+class PostPerformanceTest < ActionDispatch::PerformanceTest
+  # Refer to the documentation for all available options
+  # self.profile_options = { runs: 5, metrics: [:wall_time, :memory],
+  #                          output: 'tmp/performance', formats: [:flat] }
+
+  test "test homepage" do
+    get '/posts'
+  end
+
+  test "test creating new post" do
+    post '/posts', :post => { :title => "Hello World", :content => "How are you?" }
+  end
+end
